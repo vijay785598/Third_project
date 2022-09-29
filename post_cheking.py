ab2687648f012279e511a563dde402abff74124b
vijay785598 <114460090+vijay785598@users.noreply.github.com>
2022-09-29 13:09:45 +0530

    practice module added
    commit chenges

---
+++ post_cheking.py
@@ -0,0 +1,9 @@
+gets={
+    "caller Id": "vijay12",
+    "description": "money dedected",
+    "short_description": "ticket creation testing from service now api",
+    "id": "671",
+    "createdAt": "2022-09-27T02:46:48.573Z"
+}
+
+print(gets["caller Id"])   #out put= vijay12
